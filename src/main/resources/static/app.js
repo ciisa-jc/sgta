992496e69aba4fb65c2655f46454e4ecffdf7a4f
José Cuzmar <jose.cuzmar.zamorano@ciisa.cl>
2021-10-22 03:11:36 -0300

    add remote server

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -13,7 +13,8 @@ function setConnected(connected) {
 }
 
 function connect() {
-    var socket = new SockJS('http://localhost:8080/stomp-endpoint');
+//    var socket = new SockJS('http://localhost:8080/stomp-endpoint');
+    var socket = new SockJS('https://sgtasec.herokuapp.com/stomp-endpoint');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
